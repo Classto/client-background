7fd4e66e59a766d748e93584ac5caf8edf4ba560
Sandsunset <sejoon0606@gmail.com>
2021-10-31 19:39:39 +0900

    fix: bug

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ import json
 import requests
 
 class App:
-    def __init__(self, email, pwd):
-        user = requests.post("http://localhost:5000/auth/login",json = {"email" : email,"pw" : pwd})
-        print(user.json())
+    def __init__(self, user):
         self.session_id = user.json()["session_id"]
         self.current_category = user.json()["current_category"]
         self.meetings = requests.get(f"http://localhost:5000/schedule/{self.session_id}").json()["data"]
@@ -33,20 +31,18 @@ class App:
  
             await self.enter_zoom()
 
-print('''
-   _____ _               _        
-  / ____| |             | |       
- | |    | | __ _ ___ ___| |_ ___  
- | |    | |/ _` / __/ __| __/ _ \ 
- | |____| | (_| \__ \__ \ || (_) |
-  \_____|_|\__,_|___/___/\__\___/ 
-                                  ''')
-email = "sejoon0606@gmail.com" #input("email: ")
-pwd = "1234" #input("password: ")
-app = App(email=email, pwd=pwd)
+email = ""
+pwd = ""
+with open('config.json') as config:
+    data = json.load(config)
+    email = data["email"]
+    pwd = data["pwd"]
+
+user = requests.post("http://localhost:5000/auth/login",json = {"email" : email,"pw" : pwd})
+if not user.status_code == 200:
+    print("error: user not found")
+app = App(user)
 
 loop = asyncio.new_event_loop()
 loop.create_task(app.main())
 loop.run_forever()
-# user = requests.post("http://localhost:5000/auth/login",json = {"email" : email,"pw" : pwd})
-# print(user.json()["session_id"])
