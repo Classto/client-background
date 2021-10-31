5a79f75a3a97a060f1b1da4f12ef9004d3b5f2db
Sandsunset <sejoon0606@gmail.com>
2021-10-31 19:26:07 +0900

    v1

--- main.py
+++ main.py
@@ -1,39 +1,52 @@
-import selenium.webdriver as webdriver
 import asyncio
+import webbrowser
 from datetime import datetime
-from json import loads
+import json
 
-options = webdriver.ChromeOptions()
-# options.add_argument("headless")
-
-browser = webdriver.Chrome(executable_path="chromedriver", options=options)
-browser.get("https://classto-beta.loca.lt/")
+import requests
 
 class App:
-    async def enter_zoom(self) -> None:
+    def __init__(self, email, pwd):
+        user = requests.post("http://localhost:5000/auth/login",json = {"email" : email,"pw" : pwd})
+        print(user.json())
+        self.session_id = user.json()["session_id"]
+        self.current_category = user.json()["current_category"]
+        self.meetings = requests.get(f"http://localhost:5000/schedule/{self.session_id}").json()["data"]
+
+    async def enter_zoom(self):
         current_time = datetime.now()
         if current_time.weekday() == 6:
             weekday = "0"
         else:
             weekday = str(current_time.weekday() + 1)
 
-        # print(weekday)
-        # print(f"{current_time.hour}:{current_time.minute}")
-
         for meeting in self.meetings[self.current_category]:
-            if meeting.time == f"{current_time.hour}:{current_time.minute}" and weekday in meeting["repeating-days"]:
-                print('oh')
+            if meeting["time"] == f"{current_time.hour}:{current_time.minute}" and weekday in meeting["repeating-days"]:
+                id = meeting["id"]
+                pwd = meeting["pw"]
+                uname = meeting["nickname"]
+                webbrowser.open(f"zoommtg://zoom.us/join?action=join&confno={id}&pwd={pwd}&uname={uname}")
 
     async def main(self):
         while True:
-            self.meetings = loads(browser.execute_script('return localStorage.getItem("meetings")'))
-            self.current_category = browser.execute_script('return localStorage.getItem("recent_editor")')
             await asyncio.sleep(2)
  
             await self.enter_zoom()
 
-app = App()
+print('''
+   _____ _               _        
+  / ____| |             | |       
+ | |    | | __ _ ___ ___| |_ ___  
+ | |    | |/ _` / __/ __| __/ _ \ 
+ | |____| | (_| \__ \__ \ || (_) |
+  \_____|_|\__,_|___/___/\__\___/ 
+                                  ''')
+email = "sejoon0606@gmail.com" #input("email: ")
+pwd = "1234" #input("password: ")
+app = App(email=email, pwd=pwd)
 
 loop = asyncio.new_event_loop()
 loop.create_task(app.main())
 loop.run_forever()
+# user = requests.post("http://localhost:5000/auth/login",json = {"email" : email,"pw" : pwd})
+# print(user.json()["session_id"])
